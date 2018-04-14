03f4728679abb3e03cac00e39c376599f2537218
Marcos Barbosa <barbosa.mg@gmail.com>
2018-04-14 00:30:56 -0300

    Criando o módulo principal da aplicação e o primeiro controller

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,36 +1,10 @@
-angular.module('projeto')
-    .controller('FotoController', function($scope, $routeParams, $location, FotoService) {
+angular
+    .module('alurapic')
+    .controller('FotoController', function($scope) {
 
-        $scope.foto = {};
-        $scope.mensagem = '';
-    
-        if($routeParams.fotoId) {
-            FotoService.get({fotoId: $routeParams.fotoId}, function(retorno) {
-                 $scope.foto = retorno;
-            });
+        $scope.foto = {
+            titulo: 'Leão',
+            url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
         }
 
-        $scope.submeter = function() {
-           if($scope.formulario.$valid) {
-                if(!$routeParams.fotoId) {
-                    
-                    FotoService.save($scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Salvo com sucesso';
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível salvar';
-                    });
-
-                } else {
-                    FotoService.update({fotoId : $scope.foto._id}, $scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Alterado com sucesso';                        
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });                    
-                }
-           } 
-        };
     });
