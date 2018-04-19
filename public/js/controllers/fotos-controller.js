351b804abfd47df9b93fb9f4bdc2fedd59d83ad8
Marcos Barbosa <barbosa.mg@gmail.com>
2018-04-19 01:46:46 -0300

    Criando um serviço para manipular chamadas rest e utilizando ngResource

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,34 +1,29 @@
 angular
     .module('alurapic')
-    .controller('FotosController', function($scope, $http, $routeParams) {
+    .controller('FotosController', function($scope, $routeParams, recursoFoto) {
 
         $scope.fotos = [];
         $scope.filtro = '';
 
-        $http.get('v1/fotos')
-            .success(function(fotos) {
-                $scope.fotos = fotos;
-            })
-            .error(function(erro) {
-                console.log(erro);
-            });
+
+        recursoFoto.query(function(fotos) {
+            $scope.fotos = fotos;
+        }, function(erro) {
+            console.log(erro);
+        });
 
 
         if($routeParams.fotoId) {
 
             var fotoId = $routeParams.fotoId;
 
-            $http.get('/v1/fotos/' + fotoId)
-                .success(function(foto) {
-                    $scope.foto = foto
-                })
-                .error(function(erro) {
-                    console.log(erro);
-                });
-    
-            ;
-        }
+            recursoFoto.get({fotoId: fotoId}, function(foto) {
+                $scope.foto = foto
+            }, function(erro) {
+                $scope.flashMsg.erro = 'Não foi possível carregar a foto.'
+            });
 
+        }
 
         $scope.flashMsg = {};
 
@@ -37,45 +32,40 @@ angular
             if($scope.formulario.$valid) {
 
                 if($routeParams.fotoId) {
-                    $scope.editar($scope.foto._id, $scope.foto);
+                    $scope.editar($scope.foto);
                 } else {
-                    $http.post('/v1/fotos', $scope.foto)
-                        .success(function() {
-                            $scope.flashMsg.sucesso = 'Foto cadastrada com sucesso.';
-                            $scope.foto = {};
-                            $scope.formulario.$setPristine();
-                        })
-                        .error(function(erro) {
-                            $scope.flashMsg.erro = 'Não foi possível cadastrar a foto.'
-                        });
+                    recursoFoto.save($scope.foto, function() {
+                        $scope.flashMsg.sucesso = 'Foto cadastrada com sucesso.';
+                        $scope.foto = {};
+                        $scope.formulario.$setPristine();
+                    }, function(erro) {
+                        $scope.flashMsg.erro = 'Não foi possível cadastrar a foto.'
+                    });
                 }
 
             }
             
         }
 
-        $scope.editar = function(id, foto) {
-            $http.put('/v1/fotos/' + id, foto)
-                .success(function() {
-                    $scope.flashMsg.sucesso = 'Foto alterada com sucesso.';
-                    
-                })
-                .error(function(erro) {
-                    $scope.flashMsg.sucesso = 'Não foi possível alterar foto.';
-                });
+        $scope.editar = function(foto) {
+
+            recursoFoto.update({fotoId: foto._id}, foto, function() {
+                $scope.flashMsg.sucesso = 'Foto alterada com sucesso.';
+            }, function(erro) {
+                $scope.flashMsg.sucesso = 'Não foi possível alterar foto.';
+            });
+
         }
 
         $scope.remover = function(foto) {
             
-            $http.delete('/v1/fotos/' + foto._id)
-                .success(function() {
-                    var fotoIndex = $scope.fotos.indexOf(foto);
-                    $scope.fotos.splice(fotoIndex, 1);
-                    $scope.flashMsg.sucesso = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
-                })
-                .error(function(erro) {
-                    $scope.flashMsg.sucesso = 'Não foi possível remover foto.';
-                });
+            recursoFoto.delete({fotoId: foto._id}, function() {
+                var fotoIndex = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(fotoIndex, 1);
+                $scope.flashMsg.sucesso = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
+            }, function(erro) {
+                $scope.flashMsg.sucesso = 'Não foi possível remover foto.';
+            });
 
             return false;
         }
